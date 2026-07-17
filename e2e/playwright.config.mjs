7d88ba4173808dd34c00e133d8ed4956fc8f99d8
Lnovo <pikainjapan0003@gmail.com>
2026-07-18 02:52:13 +0800

    e2e-failure-trace-artifact

--- e2e/playwright.config.mjs
+++ e2e/playwright.config.mjs
@@ -32,6 +32,7 @@ export default defineConfig({
   reporter: "line",
   use: {
     baseURL: "http://127.0.0.1:4173",
+    screenshot: "only-on-failure",
     trace: "retain-on-failure",
   },
   webServer: ciOnlyWebServers,
